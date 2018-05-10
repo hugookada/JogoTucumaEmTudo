4cf79ef73c2568ca2f905ff0aacde26ee9206ddf
hugookada <39027314+hugookada@users.noreply.github.com>
2018-05-10 18:36:24 -0400

    Add files via upload

---
+++ Assets/script/DestroiObjeto.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroiObjeto : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Invoke("ApagaObjeto", 1.5f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+    void ApagaObjeto()
+    {
+        Destroy(this.gameObject);
+    }
+}
